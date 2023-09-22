056cddcfae199e04c9116075edef58476ce25186
joona95 <joona95@naver.com>
2023-09-22 22:28:22 +0900

    feat: Named 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -44,10 +44,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m From Member m where m.team = :team";
-
-            List<Member> result = em.createQuery(query, Member.class)
-                    .setParameter("team", teamA)
+            List<Member> result = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "member1")
                     .getResultList();
 
             for (Member member : result) {
